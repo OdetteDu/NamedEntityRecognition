2d682daba009a32de9bbba139ec4c54d101606ef
zliu24 <lucasliu94s@gmail.com>
2015-11-20 23:28:55 -0800

    Updated file from code base

--- src/cs224n/deep/ObjectiveFunction.java
+++ src/cs224n/deep/ObjectiveFunction.java
@@ -4,14 +4,15 @@ import java.lang.*;
 import org.ejml.simple.SimpleMatrix;
 
 /**
- * An objective function takes an input vector and gives an answer.
+ * An objective function takes an input vector and computes the error for a given example.
  * @author Chris Billovits
  */
 public interface ObjectiveFunction {
 
     /**
-     * Evaluates an objective function on top of a Matrix vector input.
-     */   
-    public double valueAt(SimpleMatrix input);
+     * Evaluates an objective function on top of a Matrix vector input, 
+     * against a label.
+     */
+    public double valueAt(SimpleMatrix label, SimpleMatrix input);
 
 }
